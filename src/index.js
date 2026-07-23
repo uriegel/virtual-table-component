b329b16aaf0f747df07fe5de3d292de8cd0d23cd
Uwe Riegel <uriegel@hotmail.de>
2026-07-23 10:15:36 +0200

    TODO

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 export * from './VirtualTable.js';
 
+// TODO Web Component of a virtual table (plain)
+// TODO access t in test web app
 // TODO measure one item when itemheight == 0
 // TODO html list in the web component ~ 100 items in an array
 // TODO Keyboard control and mouse scrolling, add items above and remove items below
