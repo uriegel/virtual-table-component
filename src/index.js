71b12f9bbf6ebd06430fc108e9d5cb835e951931
Uwe Riegel <uriegel@hotmail.de>
2026-07-23 10:14:52 +0200

    TODO

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 export * from './VirtualTable.js';
 
-// TODO WebserverLight
+// TODO measure one item when itemheight == 0
 // TODO html list in the web component ~ 100 items in an array
-// TODO measure one item
-// TODO First step:
-/* 
-    
-    Scrollbar web component to scroll through this list
-*/
-// TODO Keyboard control and mouse scrolling
+// TODO Keyboard control and mouse scrolling, add items above and remove items below
+// TODO Scrollbar web component to scroll through this list
 // TODO Slot to render a new cell in the program with recycling
+
+// TODO WebserverLight with website request
