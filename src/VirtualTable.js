1c7a6e9b141355297655f329e6f44c1922ffcaba
Uwe Riegel <uriegel@hotmail.de>
2026-07-23 15:21:22 +0200

    Measuring

--- src/VirtualTable.js
+++ src/VirtualTable.js
@@ -1,3 +1,12 @@
+// TODO measure one item when itemheight == 0
+// TODO table without spaces
+// TODO html list in the web component ~ 100 items in an array
+// TODO Keyboard control and mouse scrolling, add items above and remove items below
+// TODO Scrollbar web component to scroll through this list
+// TODO Slot to render a new cell in the program with recycling
+
+// TODO WebserverLight with website request
+
 export class VirtualTable extends HTMLElement {
 
     constructor() {
@@ -6,19 +15,22 @@ export class VirtualTable extends HTMLElement {
     
     connectedCallback() {
         this.shadow = this.attachShadow({ mode: "open" })
-        const main = document.createElement("div")
-        main.style.overflow = "hidden"
-        main.style.position = "absolute"
-        main.style.width = "100%"
-        main.style.height = "100%"
+        this.main = document.createElement("div")
+        this.main.style.overflow = "hidden"
+        this.main.style.position = "absolute"
+        this.main.style.width = "100%"
+        this.main.style.height = "100%"
         this.table = document.createElement("table")
         this.tableBody = document.createElement("tbody")
         this.table.appendChild(this.tableBody)
-        main.appendChild(this.table)
-        this.shadow.appendChild(main)
+        this.main.appendChild(this.table)
+        this.shadow.appendChild(this.main)
     }
 
     setItems(items) {
+
+        this.measure()
+
         while (this.tableBody.lastElementChild) 
             this.tableBody.removeChild(this.tableBody.lastElementChild)
 
@@ -29,6 +41,17 @@ export class VirtualTable extends HTMLElement {
             tr.appendChild(td)
             this.tableBody.appendChild(tr)
         })
+    }
+
+    measure() {
+        const tr = document.createElement("tr")
+        const td = document.createElement("td")
+        td.textContent = "item"
+        tr.appendChild(td)
+        this.tableBody.appendChild(tr)
+        const height = tr.offsetHeight
+        const bodyHeight = this.main.clientHeight
+        console.log(height, bodyHeight, bodyHeight/height)
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-export * from './VirtualTable.js';
+export * from './VirtualTable.js'
 
-// TODO Web Component of a virtual table (plain)
-// TODO access it in test web app
-// TODO measure one item when itemheight == 0
-// TODO html list in the web component ~ 100 items in an array
-// TODO Keyboard control and mouse scrolling, add items above and remove items below
-// TODO Scrollbar web component to scroll through this list
-// TODO Slot to render a new cell in the program with recycling
-
-// TODO WebserverLight with website request
