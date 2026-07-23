f34ad11e73c8ff8fef4e1101e260502a909c6812
Uwe Riegel <uriegel@hotmail.de>
2026-07-23 13:06:59 +0200

    Fill many elements, not virtualized, no scrolling

--- src/VirtualTable.js
+++ src/VirtualTable.js
@@ -1,14 +1,34 @@
 export class VirtualTable extends HTMLElement {
+
     constructor() {
         super()
     }
     
     connectedCallback() {
-        const shadow = this.attachShadow({ mode: "open" })
+        this.shadow = this.attachShadow({ mode: "open" })
+        const main = document.createElement("div")
+        main.style.overflow = "hidden"
+        main.style.position = "absolute"
+        main.style.width = "100%"
+        main.style.height = "100%"
+        this.table = document.createElement("table")
+        this.tableBody = document.createElement("tbody")
+        this.table.appendChild(this.tableBody)
+        main.appendChild(this.table)
+        this.shadow.appendChild(main)
+    }
 
-        const div = document.createElement("div")
-        div.textContent = "Hallo, ich bin eine Web-Komponente"
-        shadow.appendChild(div)
+    setItems(items) {
+        while (this.tableBody.lastElementChild) 
+            this.tableBody.removeChild(this.tableBody.lastElementChild)
+
+        items.forEach(item => {
+            const tr = document.createElement("tr")
+            const td = document.createElement("td")
+            td.textContent = item
+            tr.appendChild(td)
+            this.tableBody.appendChild(tr)
+        })
     }
 }
 
--- test/index.js
+++ test/index.js
@@ -1,1 +1,7 @@
 import '../src/index.js'
+
+const tableView = document.getElementById("virtual-table") 
+const fill = document.getElementById("fill")
+
+fill.onclick = () => tableView.setItems(
+    Array.from({ length: 70 }, (value, index) => `Eintrag Numero: ${index}`))
