574d80b12ed02922c1e7a5a92ba28f732b7e041d
Uwe Riegel <uriegel@hotmail.de>
2026-07-24 12:57:44 +0200

    WebServerLight

--- test/Program.cs
+++ test/Program.cs
@@ -1,17 +1,40 @@
 using System;
-using UwebServer;
-using UwebServer.Routes;
+using System.IO;
+using System.Threading.Tasks;
+using CsTools.Extensions;
+using WebServerLight;
+using WebServerLight.Routing;
 
 var port = 9882;
 
-var routeWebSite = new Static() { FilePath = "." };
-var server = new Server(new()
-{
-    Port = port,
-    Routes = new[] { routeWebSite },
-});
-server.Start();
+WebServer
+    .New()
+    .Logging(LogLevel.Trace)
+    .Http(port)
+    .Route(MethodRoute
+        .New(Method.Get)
+        .Request(GetFile))
+    .Build()
+    .Start();
+
 Console.WriteLine($"Running test server on http://localhost:{port}/test/index.html");
-Console.ReadLine();
+Console.ReadLine(); 
 
 
+static async Task<bool> GetFile(IRequest request)
+{
+    try
+    {
+        var subPath = request.Url[1..];
+        if (subPath == null)
+            return false;
+        using var stream = File.OpenRead(subPath);
+
+        await request.SendAsync(stream, stream.Length, subPath?.GetFileExtension()?.ToMimeType() ?? "text/plain", new FileInfo(stream.Name).LastWriteTime);
+        return true;
+    }
+    catch (Exception)
+    {
+        return false;
+    }
+}
