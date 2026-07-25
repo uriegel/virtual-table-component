7a68c01717a27e2a8037005b88fff6abc9d9edae
Uwe Riegel <uriegel@hotmail.de>
2026-07-25 15:43:28 +0200

    create-row and render-row

--- test/index.js
+++ test/index.js
@@ -3,6 +3,24 @@ import '../src/index.js'
 const tableView = document.getElementById("virtual-table") 
 const fill = document.getElementById("fill")
 
+tableView.addEventListener("create-rowitem", evt => {
+    const tr = document.createElement("tr")
+    const td = document.createElement("td")
+    td.id = "td"
+    tr.appendChild(td)
+    evt.detail.tr = tr
+})
+tableView.addEventListener("render-rowitem", evt => {
+
+        if (!evt.detail.item)
+            alert("Hilfe)")
+
+
+    const tr = evt.detail.tr
+    const td = tr.querySelector('#td')
+    td.textContent = `Das muss so sein: ${evt.detail.item}`
+})
+
 fill.onclick = () => {
     tableView.setItems(Array.from({ length: 70 }, (value, index) => `Eintrag Numero: ${index}`))
     tableView.focus()
