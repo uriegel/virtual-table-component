9f48590dbeffb43f703ea92887cce0ed0e9b63c1
Uwe Riegel <uriegel@hotmail.de>
2026-07-29 06:23:36 +0200

    setPosition

--- src/VirtualTable.js
+++ src/VirtualTable.js
@@ -1,9 +1,8 @@
 import './Scrollbar.js'
 
-// TODO set columns: remove old columns, reset scrollbar
-// TODO set columns: sorting items by sort function
-// TODO set items: remove old items, reset scrollbar
-// TODO css style for item row from css: call method to transfer it o the shadow dom
+// TODO columns: sorting items by sort function
+// TODO columns: custom rendering for sub sorting
+// TODO css style for item row from css: call method to transfer it to the shadow dom
 // TODO scrollbar hidden: transition
 // TODO scrollbar active transition
 
@@ -193,9 +192,21 @@ export class VirtualTable extends HTMLElement {
                 const tr = this.createItem(item, idx)
                 this.tableBody.appendChild(tr)
             })
-        if (pos || pos > 0)
-            this.checkPosition(pos)
     }
+
+    setPosition(newPos) {
+        const up = newPos < this.currentPosition
+        newPos = up ? Math.max(newPos, 0) : Math.min(newPos, this.items.length - 1)
+        const delta = this.scrollIntoView(newPos, up)
+        const elements = Array.from(this.tableBody.children)
+        const element = elements[this.currentPosition - this.offset]
+        if (element)
+            element.classList.remove("isCurrent")
+        const newElement = elements[newPos - this.offset]
+        newElement.classList.add("isCurrent")
+        this.currentPosition = newPos
+    }
+
     measure() {
         var tr = this.createRowItem()
         this.measureRowItem(tr)
@@ -293,29 +304,29 @@ export class VirtualTable extends HTMLElement {
             }
         }
         // TODO check if too small
-        this.checkPosition(this.currentPosition)
+        this.setPosition(this.currentPosition)
     }
 
     onKeyDown(evt) {
         if (evt.key == "ArrowDown") {
             evt.preventDefault()
             evt.stopPropagation()
-            this.checkPosition(this.currentPosition + 1)
+            this.setPosition(this.currentPosition + 1)
         }
         else if (evt.key == "ArrowUp") {
             evt.preventDefault()
             evt.stopPropagation()
-            this.checkPosition(this.currentPosition - 1)
+            this.setPosition(this.currentPosition - 1)
         }
         else if (evt.key == "PageDown") {
             evt.preventDefault()
             evt.stopPropagation()
-            this.checkPosition(this.currentPosition + this.visualItemsCount - 1)
+            this.setPosition(this.currentPosition + this.visualItemsCount - 1)
         }
         else if (evt.key == "PageUp") {
             evt.preventDefault()
             evt.stopPropagation()
-            this.checkPosition(this.currentPosition - this.visualItemsCount + 1)
+            this.setPosition(this.currentPosition - this.visualItemsCount + 1)
         }
         else if (evt.key == "End") {
             evt.preventDefault()
@@ -373,19 +384,6 @@ export class VirtualTable extends HTMLElement {
             detail: { pos: this.currentPosition }
         })
         this.dispatchEvent(event)
-    }
-
-    checkPosition(newPos) {
-        const up = newPos < this.currentPosition
-        newPos = up ? Math.max(newPos, 0) : Math.min(newPos, this.items.length - 1)
-        const delta = this.scrollIntoView(newPos, up)
-        const elements = Array.from(this.tableBody.children)
-        const element = elements[this.currentPosition - this.offset]
-        if (element)
-            element.classList.remove("isCurrent")
-        const newElement = elements[newPos - this.offset]
-        newElement.classList.add("isCurrent")
-        this.currentPosition = newPos
     }
 
     scrollIntoView(newPos, up) {
