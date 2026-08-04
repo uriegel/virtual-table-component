61ac8dc08d749d0cb095f94528c3eeb45edf514e
Uwe Riegel <uriegel@hotmail.de>
2026-08-04 06:20:05 +0200

    Problem with mousedown

--- src/VirtualTable.js
+++ src/VirtualTable.js
@@ -70,7 +70,6 @@ export class VirtualTable extends HTMLElement {
         this.root.id = "root"
         this.root.setAttribute("tabindex", "0")
         this.root.addEventListener("keydown", evt => this.onKeyDown(evt))
-        this.root.addEventListener("mousedown", evt => this.onMouseDown(evt))
         this.root.addEventListener("dblclick", () => this.onSelected())
         this.root.addEventListener("wheel", evt => this.onWheel(evt))
         this.table = document.createElement("table")
@@ -80,6 +79,7 @@ export class VirtualTable extends HTMLElement {
         this.tableHead.appendChild(this.tableHeadRow)
         this.table.appendChild(this.tableHead)
         this.tableBody = document.createElement("tbody")
+        this.tableBody.addEventListener("mousedown", evt => this.onMouseDown(evt))
         this.table.appendChild(this.tableBody)
         this.root.appendChild(this.table)
         this.scrollbar = document.createElement("scroll-bar")
